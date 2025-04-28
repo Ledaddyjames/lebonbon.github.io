581359cd6614812a14773dda9ca53d0c11eaf595
Ledaddyjames <1054357@lwsd.org>
2025-04-28 13:58:05 -0700

    Update server.js

--- server.js
+++ server.js
@@ -13,3 +13,34 @@ app.get('*', (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+const express = require('express');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files (like index.html)
+app.use(express.static('public'));
+
+// Simple Proxy Endpoint
+app.get('/proxy', async (req, res) => {
+    const targetUrl = req.query.url;
+    
+    if (!targetUrl) {
+        return res.status(400).send('No URL specified');
+    }
+
+    try {
+        const response = await fetch(targetUrl);
+        const body = await response.text();
+        res.send(body);
+    } catch (error) {
+        res.status(500).send('Error fetching the URL');
+    }
+});
+
+// Start Server
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
